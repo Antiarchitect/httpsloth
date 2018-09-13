c4ec8e1d41739eb2938adceb642adef659411ee5
Andrey Voronkov <voronkovaa@gmail.com>
2018-09-13 13:24:54 +0300

    Optimize Stream module a little.

--- src/stream.rs
+++ src/stream.rs
@@ -3,17 +3,20 @@ use std::io::{self, Read, Write};
 use super::tokio_tls::TlsStream;
 use super::tokio_core::net::TcpStream;
 
+
 pub enum MaybeHttpsStream {
     Http(TcpStream),
     Https(TlsStream<TcpStream>),
 }
 
+use MaybeHttpsStream::*;
+
 impl Read for MaybeHttpsStream {
     #[inline]
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        match *self {
-            MaybeHttpsStream::Http(ref mut s) => s.read(buf),
-            MaybeHttpsStream::Https(ref mut s) => s.read(buf),
+        match self {
+            Http(ref mut stream) => stream.read(buf),
+            Https(ref mut stream) => stream.read(buf),
         }
     }
 }
@@ -21,17 +24,17 @@ impl Read for MaybeHttpsStream {
 impl Write for MaybeHttpsStream {
     #[inline]
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        match *self {
-            MaybeHttpsStream::Http(ref mut s) => s.write(buf),
-            MaybeHttpsStream::Https(ref mut s) => s.write(buf),
+        match self {
+            Http(ref mut s) => s.write(buf),
+            Https(ref mut s) => s.write(buf),
         }
     }
 
     #[inline]
     fn flush(&mut self) -> io::Result<()> {
-        match *self {
-            MaybeHttpsStream::Http(ref mut s) => s.flush(),
-            MaybeHttpsStream::Https(ref mut s) => s.flush(),
+        match self {
+            Http(ref mut s) => s.flush(),
+            Https(ref mut s) => s.flush(),
         }
     }
 }
